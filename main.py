7c673d319c6ec0c25c169f85349df5bbb30f5776
spencer <spencer.m.owens@gmail.com>
2025-01-18 20:28:01 -0800

    proc

--- main.py
+++ main.py
@@ -203,4 +203,5 @@ async def test_cors():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
